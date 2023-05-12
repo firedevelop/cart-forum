bfe3134822f85acbbe3100be90182c356b070c18
firedevelop <firedevelop.com@gmail.com>
2023-05-12 12:08:16 +0200

    .
    .

--- js/1.js
+++
@@ -1,8 +0,0 @@
-function eliminarCurso(e){
-    e.preventDefault()
-    if(e.target.classList.contains('borrar-curso')){
-        const cursoId = e.target.getAttribute('data-id')
-        articulosCarrito = articulosCarrito.filter( x = => cursoId.id !== cursoId)
-        carritoHTML()
-    }
-}
